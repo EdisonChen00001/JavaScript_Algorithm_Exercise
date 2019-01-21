dc53bd6cf5f4a11a6b13ee1308adb3624378c107
zhouH0901 <zhezhou@my.bridgeport.edu>
2019-01-20 23:00:32 -0500

    review maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+
+    const arr = {}
+   
+
+    for (let character of str){
+        if(character in arr){
+            arr[character] ++
+        }else{
+            arr[character] = 1
+        }
+    }
+
+let  max = 0;
+let  maxChar = '';
+
+    for(let char in arr){
+        if(arr[char]> max){
+            max = arr[char];
+            maxChar = char;
+        }
+    }
+    return maxChar
+}
 
 module.exports = maxChar;
