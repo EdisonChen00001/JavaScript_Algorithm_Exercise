023f33885d820501cc11ea6d1dd491c3a4932fcf
zhouH0901 <zhezhou@my.bridgeport.edu>
2019-01-21 18:35:54 -0500

    review chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -12,19 +12,17 @@
 
 // chunk([1, 2, 3, 4], 2) 
 function chunk(array, size) {
-    
-    const chunked = []
-    for (let element of array){
-        const last = chunked[chunked.length -1];
-        if(!last || last.length === size){
-            chunked.push([element]);
-        }else{
-            last.push(element)
-        }
-    }
-    return chunked
+   const chunked = []
+   for (let element of array){
+       const last = chunked[chunked.length-1];
+       if(!last || last.length === size){
+           chunked.push([element])
+       }else{
+           last.push(element)
+       }
+   }
 
-
+   return chunked
 }
 
 module.exports = chunk;
