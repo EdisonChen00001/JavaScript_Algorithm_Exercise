ea918ee73db7173cb9406d2182c87fc60ef6d0dc
zhouH0901 <zhezhou@my.bridgeport.edu>
2019-01-23 01:09:52 -0500

    soultion by my side not good one

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,47 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    strA = stringA.replace(/[^\w]/g,"").toLowerCase();
+    strB = stringB.replace(/[^\w]/g,"").toLowerCase();
+    listA = {};
+    listB = {};
+    for(let element of stringA){
+        if(element in listA){
+            listA[element]++;
+        }else{
+            listA[element] = 1;
+        }
+    }
+    for(let element of stringB){
+        if(element in listB){
+            listB[element]++;
+        }else{
+            listB[element] = 1;
+        }
+    }
+
+    for(let element in listA){
+        if(element in listB){
+          if(listA[element] !== listB[element]){
+            return false
+          }
+        }else{
+          return false
+        }
+      }
+  
+    for(let element in listB){
+        if(element in listA){
+          if(listA[element] !== listB[element]){
+            return false
+          }
+        }else{
+          return false
+        }
+      }
+
+    return true
+}
 
 module.exports = anagrams;
