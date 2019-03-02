3acc11dbb7034346e538f35a160876a94df8bce1
zhouH0901 <zhezhou@my.bridgeport.edu>
2019-03-01 20:18:04 -0500

    feb 28

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,15 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    let num = 0;
+    const store = ['a','e','i','o','u'];
+    for(let word of str.toLowerCase()){
+        if(store.includes(word)){
+            num++
+        }
+    }
+    return num;
+}
 
 module.exports = vowels;
